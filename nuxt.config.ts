9490dd6ddd1d918b8677c517669f712d6ab077cc
FlameyFox <flameblader95@live.dk>
2023-08-13 10:02:14 +0200

    added sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss", "@nuxtjs/robots"],
+  modules: [
+    "@nuxtjs/supabase",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/robots",
+    "nuxt-simple-sitemap",
+  ],
   devtools: { enabled: true },
   plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
   supabase: {
